0a344c82162bda65afe5637bdbeaf97ef4365367
HwangSoojin <blue8959@naver.com>
2019-11-05 16:11:15 +0900

    Update spell_checker.py

--- src/hanspell/spell_checker.py
+++ src/hanspell/spell_checker.py
@@ -72,43 +72,4 @@ def check(text):
         'words': OrderedDict(),
     }
 
-    # 띄어쓰기로 구분하기 위해 태그는 일단 보기 쉽게 바꿔둠.
-    # ElementTree의 iter()를 써서 더 좋게 할 수 있는 방법이 있지만
-    # 이 짧은 코드에 굳이 그렇게 할 필요성이 없으므로 일단 문자열을 치환하는 방법으로 작성.
-    # html = html.replace('<span class=\'re_green\'>', '<green>') \
-    #            .replace('<span class=\'re_red\'>', '<red>') \
-    #            .replace('<span class=\'re_purple\'>', '<purple>') \
-    #            .replace('</span>', '<end>')
-    # items = html.split(' ')
-    # words = []
-    # tmp = ''
-    # for word in items:
-    #     if tmp == '' and word[:1] == '<':
-    #         pos = word.find('>') + 1
-    #         tmp = word[:pos]
-    #     elif tmp != '':
-    #         word = u'{}{}'.format(tmp, word)
-    #
-    #     if word[-5:] == '<end>':
-    #         word = word.replace('<end>', '')
-    #         tmp = ''
-    #
-    #     words.append(word)
-    #
-    # for word in words:
-    #     check_result = CheckResult.PASSED
-    #     if word[:5] == '<red>':
-    #         check_result = CheckResult.WRONG_SPELLING
-    #         word = word.replace('<red>', '')
-    #     elif word[:7] == '<green>':
-    #         check_result = CheckResult.WRONG_SPACING
-    #         word = word.replace('<green>', '')
-    #     elif word[:8] == '<purple>':
-    #         check_result = CheckResult.AMBIGUOUS
-    #         word = word.replace('<purple>', '')
-    #
-    #     result['words'][word] = check_result
-
-    # result = Checked(**result)
-
     return result['checked']
